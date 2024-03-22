d935dfe8ac079c905600dca1c984defff58056b1
booker1997 <booker.schelhaas@gmail.com>
2024-03-22 15:24:21 -0400

    booker updates

--- google_gui/widgets.py
+++ google_gui/widgets.py
@@ -1,36 +1,81 @@
-from tkinter import *
+
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 from tkVideoPlayer import TkinterVideo
 from datetime import timedelta
+import customtkinter
 from reba_video_analyzer import *
-
-
+import math
+import time
+import threading
 # print(peaks_dataframe)
 class VideoWidget():
     def __init__(self,gui_dataframe_output,peaks_dataframe,total_dataframe,video_file_path=None):
-        self.root = tk.Tk()
+        # self.root = tk.Tk()
+        #style
+        self.root = customtkinter.CTk()
+        bg_color = "light grey"
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        self.root.geometry(f"{screen_width}x{screen_height}")
+        self.root.config(bg=bg_color)
+        self.root.state("zoomed")
+        self.root.resizable(False, False)
+        self.style = ttk.Style()
+        self.style.configure("Custom.TFrame", background=bg_color)
+        self.style.configure("Custom.TButton", background=bg_color, font=("Helvetica", 30))
+        self.style.configure("Custom.TLabel", background=bg_color, foreground="black", font=("Helvetica", 30))
+
+        # Variables
+        self.timer_length = 200
+        self.timer_seconds = 0
+        self.timer_running = False
+        self.display = None
+
         self.gui_dataframe_output = gui_dataframe_output
         self.peaks_dataframe = peaks_dataframe
         self.total_dataframe=total_dataframe
-
+        print(self.gui_dataframe_output)
         # print(self.peaks_dataframe['frame_of_max_peak'])
         # print(self.peaks_dataframe['frame_c_score'])
         self.peak_frames = []
         self.peak_reba_cs = []
         self.total_frames = len(total_dataframe['frame'])
-        for i,frame in enumerate(self.gui_dataframe_output['peaks_i']):
-            pass
-        #     self.play_pause_btn = tk.Button(self.root, text="Play", command=self.play_pause)
-        #     self.play_pause_btn.pack()
-        # print(self.peak_frames,self.peak_reba_cs)
-        self.perc_btn = tk.Button(self.root, text="go to perc", command=self.seek_from_frame)
+        event_data = {}
+        for i,peak_frame in enumerate(gui_dataframe_output['peaks_i']):
+            button_data = {}
+            peak_val = gui_dataframe_output['peak_val'][i]
+            problem_body_part = self.find_problem_joint(peak_frame)
+            button_data['peak_val'] = peak_val
+            button_data['perc_in_vid'] = (peak_frame+1)/self.total_frames
+            button_data['bad_joint'] = None
+            button_data['text'] = f"Reba score: {peak_val}. {problem_body_part[:-6]} in dangerous position"
+            event_data[f'Event {i+1}'] = button_data
+
+
+        # textOption = {
+        #     "Event 1": {'text':"Left Knee in dangerous position",
+        #     "Event 2": "Right Ankle in dangerous position",
+        #     "Event 3": "Left Elbow in dangerous position"
+        # }
+
+        
+        # self.configure_rows_columns()
+        
+        
+        self.perc_btn = tk.Button(self.root, text="go to perc", command=lambda: self.seek_from_perc(self.gui_dataframe_output['peaks_i'][1]/self.total_frames))
         self.perc_btn.pack()
 
         self.root.title("Need Finder Tool Interface")
-        self.root.geometry("800x700+290+10")
+        # self.root.geometry("800x700+290+10")
+        self.countdown_var = tk.StringVar()
+        self.countdown_var_2 = tk.StringVar()
+        countdown_var_label_2 = ttk.Label(self.root, textvariable=self.countdown_var_2, style="Custom.TLabel")
+        countdown_var_label_2.pack()
+        # countdown_var_label_2.grid(row=0, column=0, columnspan=3, pady=10)
 
-        
+        self.start_countdown(self.timer_length) 
 
         self.load_btn = tk.Button(self.root, text="Load", command=self.load_video)
         self.load_btn.pack()
@@ -41,6 +86,8 @@ class VideoWidget():
         self.play_pause_btn = tk.Button(self.root, text="Play", command=self.play_pause)
         self.play_pause_btn.pack()
 
+        self.add_button(event_data, self.root)
+
         self.skip_plus_5sec = tk.Button(self.root, text="Skip -5 sec", command=lambda: self.skip(-5))
         self.skip_plus_5sec.pack(side="left")
 
@@ -49,9 +96,11 @@ class VideoWidget():
 
         self.progress_value = tk.DoubleVar(self.root)
 
-        self.progress_slider = tk.Scale(self.root, variable=self.progress_value, from_=0, to=0, orient="horizontal", command=self.seek)
+        self.progress_slider = tk.Scale(self.root, variable=self.progress_value, from_=0, to=0, orient="horizontal",command=self.seek)
         # progress_slider.bind("<ButtonRelease-1>", seek)
         self.progress_slider.pack(side="left", fill="x", expand=True)
+
+        
 
         self.end_time = tk.Label(self.root, text=str(timedelta(seconds=0)))
         self.end_time.pack(side="left")
@@ -67,14 +116,80 @@ class VideoWidget():
 
         self.skip_plus_5sec = tk.Button(self.root, text="Skip +5 sec", command=lambda: self.skip(5))
         self.skip_plus_5sec.pack(side="left")
+        self.vid_player.play()
+    def find_problem_joint(self,peak_frame):
+        high_score = 0
+        high_body_part = None
+        for key in self.total_dataframe:
+            if key[-5:] == 'score' and key[0] not in ['a','b','c']:
+                print(key,self.total_dataframe[key][peak_frame],high_score)
+                if self.total_dataframe[key][peak_frame] > high_score:
+                    high_score = self.total_dataframe[key][peak_frame]
+                    high_body_part = key
 
-        
+        return high_body_part
 
+    def format_time(self, seconds):
+        minutes, seconds = divmod(seconds, 60)
+        return f"{minutes:02}:{seconds:02}"
+    def text_display_and_seek(self, button_data, button_number, frame):
+        if self.display != None:
+            self.display.destroy()
+        perc = button_data[button_number]['perc_in_vid']
+        print(perc)
+        self.seek_from_perc(perc)
+        self.display = ttk.Label(frame, text=button_data[button_number]['text'], style="Custom.TLabel")
+        self.display.pack()
+        # self.display.grid(row=5, column=1)
+    def add_button(self, data, frame):
+        button = tk.IntVar()
+        button_counter = 1
+        for entry in data:
+            button_text = str(f"Event {button_counter}")
+            # TO VERIFY INPUT
+            # print(f"This is button {button_counter} with text {button_text} which should correspond to {data[button_text]}")
+            new_button = tk.Radiobutton(frame, bg="light blue", text=button_text, indicatoron=False, value=button_counter, variable=button, command=(lambda button_label=button_text: self.text_display_and_seek(data, button_label, frame)), font=("Helvetica", 30))
+            new_button.pack(side="left")
+            # new_button.grid(row=(button_counter + 5), column=0, pady=2)
+            button_counter += 1
+        return None
+    def seek_from_perc(self,perc):
+        # percentage = (desired_frame+1)/self.total_frames
+        # rounded_frame = int(self.total_frames*percentage)
+        seconds = perc*self.duration
+        # rounded_frame = int(self.total_frames*percentage)
+        val =int(math.floor(seconds))
+        print(self.duration,seconds,val)
+        self.vid_player.seek(val)
+        self.progress_value.set(val)
+        diff = seconds - val
+        print('before',self.vid_player.current_duration())
+        self.vid_player.play()
+        time.sleep(diff)
+        print('after',self.vid_player.current_duration())
+        self.play_pause()
+        # self.play_pause()
+    def start_countdown(self, seconds):
+        self.timer_seconds = seconds
+        self.timer_running = True
+
+        def countdown():
+            while self.timer_seconds > 0 and self.timer_running:
+                self.countdown_var.set("Time Remaining: " + self.format_time(self.timer_seconds))
+                self.countdown_var_2.set("Time Remaining: " + self.format_time(self.timer_seconds))
+                time.sleep(1)
+                self.timer_seconds -= 1
+
+            if self.timer_running:
+                self.root.withdraw()
+                
+
+        threading.Thread(target=countdown).start()
     def update_duration(self,event):
         """ updates the duration after finding the duration """
-        duration = self.vid_player.video_info()["duration"]
-        self.end_time["text"] = str(timedelta(seconds=duration))
-        self.progress_slider["to"] = duration
+        self.duration = self.vid_player.video_info()["duration"]
+        self.end_time["text"] = str(timedelta(seconds=self.duration))
+        self.progress_slider["to"] = self.duration
 
 
     def update_scale(self,event):
@@ -97,12 +212,7 @@ class VideoWidget():
             self.play_pause_btn["text"] = "Play"
             self.progress_value.set(0)
             print(self.vid_player.video_info()['framerate'])
-    def seek_from_frame(self,desired_frame=110):
-        # percentage = (desired_frame+1)/self.total_frames
-        # rounded_frame = int(self.total_frames*percentage)
-        seconds = (desired_frame+1)/29
-        # rounded_frame = int(self.total_frames*percentage)
-        self.vid_player.seek(seconds)
+    
 
 
     def seek(self,value):
